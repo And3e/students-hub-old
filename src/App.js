50d1fd4ecbcf2ec0de1bf9b472ac0e1721fe2e95
André <and3e@outlook.it>
2023-03-07 10:29:53 +0100

    first carousel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import MainNav from './navbar/navbar.js'
+import MainNav from './all/navbar.js'
+import DarkVariantExample from './home/carousel.js'
 
 function App() {
   return (
     <>
       <MainNav></MainNav>
+      <DarkVariantExample></DarkVariantExample>
     </>
   )
 }
