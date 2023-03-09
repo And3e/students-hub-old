479eed5cde0ebcfd641b04fb23a244e01e262f18
And3e <and3e@outlook.it>
2023-03-09 23:37:09 +0100

    create new navbar

--- src/all/navbar.js
+++ src/all/navbar.js
@@ -3,52 +3,79 @@ import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import { useEffect } from 'react'
+
+7
 
 /* imgs */
+import logo_scritta from './../img/logos/logo-scritta-tr.png'
 import logo from './../img/logos/logo-tr.png'
 
+/* logos */
+import { BellFill } from 'react-bootstrap-icons'
+
 function MainNav() {
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 991) {
+        document.querySelector('.logo-scritta').style.display = 'none'
+        document.querySelector('.logo').style.display = 'initial'
+      } else {
+        document.querySelector('.logo-scritta').style.display = 'initial'
+        document.querySelector('.logo').style.display = 'none'
+      }
+    }
+
+    handleResize()
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
     <Navbar bg='light' expand='lg'>
       <Container fluid>
-        <Navbar.Brand href='#home'>
+        <Navbar.Brand href='#home' className='logo-scritta'>
           <img
-            src={logo}
-            width='30'
+            src={logo_scritta}
             height='30'
-            className='d-inline-block align-top'
+            className='d-inline-block align-center'
             alt='Students Hub logo'
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls='navbarScroll' />
-        <Navbar.Collapse id='navbarScroll'>
+        <Container fluid className='navbar-container'>
+          <Navbar.Toggle className='ml' height='30' />
+          <Navbar.Collapse>
+            <Form className='d-flex'>
+              <Form.Control
+                type='search'
+                placeholder='Cerca qualcosa...'
+                className='me-2'
+                aria-label='Cerca qualcosa...'
+              />
+              <Button variant='outline-primary'>Cerca</Button>
+            </Form>
+          </Navbar.Collapse>
+          <Navbar.Brand href='#home' className='logo'>
+            <img
+              src={logo}
+              height='30'
+              className='d-inline-block align-center'
+              alt='Students Hub logo'
+            />
+          </Navbar.Brand>
           <Nav
-            className='me-auto my-2 my-lg-0'
+            className='my-2 my-lg-0'
             style={{ maxHeight: '100px' }}
             navbarScroll>
-            <Nav.Link href='#action1'>Home</Nav.Link>
-            <NavDropdown title='Link' id='navbarScrollingDropdown'>
-              <NavDropdown.Item href='#action3'>Action</NavDropdown.Item>
-              <NavDropdown.Item href='#action4'>
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href='#action5'>
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Button width='30px'>
+              <BellFill />
+            </Button>
           </Nav>
-          <Form className='d-flex'>
-            <Form.Control
-              type='search'
-              placeholder='Search'
-              className='me-2'
-              aria-label='Search'
-            />
-            <Button variant='outline-primary'>Search</Button>
-          </Form>
-        </Navbar.Collapse>
+        </Container>
       </Container>
     </Navbar>
   )
