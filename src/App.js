a4c9e81b8c1fb98a8954360109458fe0de4fccf8
And3e <and3e@outlook.it>
2023-03-16 00:03:30 +0100

    implements react-router-dom (not working)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MainNavbar from './all/navbar.js'
-
 import './index.css'
+import Bacheca from './pages/bacheca.js'
 
 function App() {
   return (
-    <>
-      <MainNavbar></MainNavbar>
-    </>
+    <Router>
+      <MainNavbar />
+      <Routes>
+        <Route path='/dashboard' component={<Bacheca />} exact />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/bacheca.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MainNavbar from './../all/navbar.js'
+
+// import Pagina from './pagina'
+// return <Navbar pagName='bacheca pagina={Pagina} />
+
+function Bacheca() {
+  return <MainNavbar pagName={'/dashboard'} />
+}
+
+export default Bacheca
