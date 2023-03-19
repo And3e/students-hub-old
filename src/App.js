7c75a8cb9f23f296138ae668a57dda06ffa7125b
And3e <and3e@outlook.it>
2023-03-19 17:39:52 +0100

    update routes

--- src/App.js
+++ src/App.js
@@ -19,11 +19,21 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Navigate to='/dashboard' />} />
+        <Route path='/dashboard' element={<MainNavbar pageID={0} />} exact />
+        <Route path='/resources' element={<MainNavbar pageID={1} />} exact />
+        <Route path='/lessons' element={<MainNavbar pageID={2} />} exact />
+        <Route path='/lessons/give' element={<MainNavbar pageID={2} />} exact />
         <Route
-          path='/dashboard'
-          element={<MainNavbar pagName='/dashboard' />}
+          path='/lessons/receive'
+          element={<MainNavbar pageID={2} />}
           exact
         />
+        <Route path='/calendar' element={<MainNavbar pageID={3} />} exact />
+        <Route path='/credits' element={<MainNavbar pageID={4} />} exact />
+
+        <Route path='/info' element={<MainNavbar pageID={5} />} exact />
+        <Route path='/account' element={<MainNavbar pageID={6} />} exact />
+
         <Route path='/admin' element={<div>Admin</div>} exact />
       </Routes>
     </Router>
