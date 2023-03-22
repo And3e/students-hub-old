6f729452de3bfbade7dd35e759d7b673ef5b6569
And3e <and3e@outlook.it>
2023-03-22 22:23:41 +0100

    implements calendar

--- src/pages/calendar/calendar.js
+++ src/pages/calendar/calendar.js
@@ -1,35 +1,75 @@
-import React, { useState } from 'react'
-import { Calendar } from '@mantine/dates'
-import { addDays, format } from 'date-fns'
+import { useState } from 'react'
+import { Group, Indicator } from '@mantine/core'
+import { DatePicker } from '@mantine/dates'
+import 'dayjs/locale/it'
 
-export default function CalendarPage() {
-  const [events, setEvents] = useState([])
-
-  function handleDayClick(date) {
-    const title = prompt('Enter the event title:')
-    if (title) {
-      const start = date
-      const end = addDays(date, 1)
-      const newEvent = { title, start, end }
-      setEvents([...events, newEvent])
-    }
-  }
-
-  function renderEvent(event) {
-    const { title, start } = event
-    const formattedDate = format(start, 'yyyy-MM-dd')
-    return (
-      <div key={title} style={{ marginTop: '200px' }}>
-        <span>{title}</span>
-        <span>{formattedDate}</span>
-      </div>
-    )
-  }
+function CalendarPage() {
+  const [value, setValue] = useState(new Date().getTime() + 24 * 60 * 60 * 1000)
 
   return (
-    <>
-      <Calendar onDayClick={handleDayClick} events={events} />
-      {renderEvent}
-    </>
+    <Group position='center'>
+      <DatePicker
+        allowDeselect
+        value={value}
+        onChange={setValue}
+        minDate={new Date()}
+        defaultValue={new Date()}
+        size='md'
+        locale='it'
+        renderDay={(date) => {
+          const day = date.getDate()
+          return (
+            <Indicator size={6} color='red' offset={-5} disabled={day !== 16}>
+              <div>{day}</div>
+            </Indicator>
+          )
+        }}
+        getDayProps={(date) => {
+          if (
+            date.getFullYear() === new Date().getFullYear() &&
+            date.getMonth() === new Date().getMonth() &&
+            date.getDate() === new Date().getDate()
+          ) {
+            return {
+              sx: (theme) => ({
+                backgroundColor: theme.colors.green[theme.fn.primaryShade()],
+                color: theme.white + ' !important',
+                ...theme.fn.hover({ backgroundColor: theme.colors.green[7] }),
+              }),
+            }
+          }
+          return {}
+        }}
+        getMonthControlProps={(date) => {
+          if (
+            date.getFullYear() === new Date().getFullYear() &&
+            date.getMonth() === new Date().getMonth()
+          ) {
+            return {
+              sx: (theme) => ({
+                backgroundColor: theme.colors.green[theme.fn.primaryShade()],
+                color: theme.white,
+                ...theme.fn.hover({ backgroundColor: theme.colors.green[7] }),
+              }),
+            }
+          }
+          return {}
+        }}
+        getYearControlProps={(date) => {
+          if (date.getFullYear() === new Date().getFullYear()) {
+            return {
+              sx: (theme) => ({
+                backgroundColor: theme.colors.green[theme.fn.primaryShade()],
+                color: theme.white,
+                ...theme.fn.hover({ backgroundColor: theme.colors.green[7] }),
+              }),
+            }
+          }
+          return {}
+        }}
+      />
+    </Group>
   )
 }
+
+export default CalendarPage
